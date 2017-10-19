a83e17682fb477dd778a47d36cb7da8d7576fab8
Devon Boyer <devonboyer@users.noreply.github.com>
2017-10-18 22:28:13 -0400

    Add airtbale package (#1)

---
+++ airtable/api.go
@@ -0,0 +1,70 @@
+package airtable
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Client struct {
+	apiKey   string
+	basePath string
+	hc       *http.Client
+}
+
+func New(apiKey string) *Client {
+	return &Client{
+		apiKey:   apiKey,
+		basePath: fmt.Sprintf("https://api.airtable.com/v%s", majorAPIVersion),
+		hc:       http.DefaultClient,
+	}
+}
+
+type Error struct {
+	StatusCode int
+	Type       string `json:"type"`
+	Message    string `json:"message"`
+	Body       string
+}
+
+func (e *Error) Error() string {
+	if e.Message != "" {
+		return fmt.Sprintf("%s (%d): %s", e.Type, e.StatusCode, e.Message)
+	}
+	return fmt.Sprintf("Unknown (%d): %s", e.StatusCode, e.Body)
+}
+
+type errorReply struct {
+	Error *Error `json:"error"`
+}
+
+const majorAPIVersion = "0"
+
+var xVersionHeader = fmt.Sprintf("%s.1.0", majorAPIVersion)
+
+func setVersionHeader(headers http.Header) {
+	headers.Set("x-api-version", xVersionHeader)
+}
+
+func checkResponse(res *http.Response) error {
+	if res.StatusCode >= 200 && res.StatusCode <= 299 {
+		return nil
+	}
+	slurp, err := ioutil.ReadAll(res.Body)
+	if err == nil {
+		jerr := new(errorReply)
+		err = json.Unmarshal(slurp, jerr)
+		if err == nil && jerr.Error != nil {
+			if jerr.Error.StatusCode == 0 {
+				jerr.Error.StatusCode = res.StatusCode
+			}
+			jerr.Error.Body = string(slurp)
+			return jerr.Error
+		}
+	}
+	return &Error{
+		StatusCode: res.StatusCode,
+		Body:       string(slurp),
+	}
+}
---
+++ airtable/table_test.go
@@ -0,0 +1,46 @@
+package airtable
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTableListCall(t *testing.T) {
+	var tests = []struct {
+		desc string
+		call *TableListCall
+		qs   string
+	}{
+		{
+			"no options",
+			newTableListCall(),
+			"",
+		},
+		{
+			"many list options",
+			newTableListCall().
+				Fields([]string{"foo", "bar"}).
+				FilterByFormula("foo").
+				MaxRecords(100).
+				PageSize(10).
+				SortFields([]SortField{SortField{"foo", true}, SortField{"bar", false}}).
+				View("foo"),
+			"fields%5B%5D=foo&fields%5B%5D=bar&filterByFormula=foo&maxRecords=100&pageSize=10&sort%5B0%5D%5Bdirection%5D=desc&sort%5B0%5D%5Bfield%5D=foo&sort%5B1%5D%5Bdirection%5D=asc&sort%5B1%5D%5Bfield%5D=bar&view=foo",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			require.Equal(t, test.qs, test.call.urlParams.Encode())
+		})
+	}
+}
+
+func newTableListCall() *TableListCall {
+	return &TableListCall{
+		client:    &Client{},
+		urlParams: make(url.Values),
+	}
+}
