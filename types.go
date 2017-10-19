fbebd7cfc95c0cc87f5f9b07033bead2916ba7fa
Devon Boyer <devonboyer94@gmail.com>
2017-10-18 22:30:24 -0400

    Update Show type.

--- types.go
+++ types.go
@@ -5,11 +5,14 @@ import "time"
 type Show struct {
 	ID     string `json:"id"`
 	Fields struct {
-		Name        string   `json:"Name"`
-		DayOfWeek   string   `json:"Day of Week"`
-		Genres      []string `json:"Genres"`
-		RunningTime string   `json:"Running Time"`
-		Networks    []string `json:"Networks"`
+		Name           string   `json:"Name"`
+		Notes          string   `json:"Notes"`
+		DayOfWeek      string   `json:"Day of Week"`
+		Genres         []string `json:"Genres"`
+		RunningTime    string   `json:"Running Time"`
+		Status         string   `json:"Status"`
+		Networks       []string `json:"Networks"`
+		PersonalRating string   `json:"Personal Rating"`
 	} `json:"fields"`
 	CreatedTime time.Time `json:"createdTime"`
 }
