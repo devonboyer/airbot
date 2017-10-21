8e2380a33d53a8a068716e49627e2da00505d389
Devon Boyer <devonboyer94@gmail.com>
2017-10-21 15:32:19 -0400

    Webhook handler.

--- messenger/webhook.go
+++ messenger/webhook.go
@@ -1,23 +1,66 @@
 package messenger
 
-import "net/http"
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func (c *Client) WebhookHandler() http.HandlerFunc {
-
 	return func(w http.ResponseWriter, req *http.Request) {
 		switch req.Method {
 		case "GET":
 			// Handle verification request.
 			if req.FormValue("hub.verify_token") == c.verifyToken {
 				w.Write([]byte(req.FormValue("hub.challenge")))
+				w.WriteHeader(http.StatusOK)
 				return
 			}
 			w.WriteHeader(http.StatusUnauthorized)
 		case "POST":
-			// Handle events.
+			// Handle event.
+			defer req.Body.Close()
 
-			// process this
-			// send to receiver channel
+			// Read body
+			body, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			// Validate event.
+			if !verifySignature(c.appSecret, body, req.Header.Get("X-Hub-Signature")[5:]) {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			var res = &ReceiveBody{}
+			err = json.NewDecoder(req.Body).Decode(res)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			// TODO: What now?
+
+			w.WriteHeader(http.StatusOK)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
 	}
+}
+
+func verifySignature(appSecret string, bytes []byte, expectedSignature string) bool {
+	if expectedSignature == "" {
+		return false
+	}
+	mac := hmac.New(sha1.New, []byte(appSecret))
+	mac.Write(bytes)
+	if fmt.Sprintf("%x", mac.Sum(nil)) != expectedSignature {
+		return false
+	}
+	return true
 }
