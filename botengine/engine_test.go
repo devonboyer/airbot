4bb10fcf103416bd34aefd9aba7fc98c6b449565
Devon Boyer <devonboyer94@gmail.com>
2017-10-24 23:06:40 -0400

    Mock sink and source for testing

---
+++ botengine/engine_test.go
@@ -0,0 +1,30 @@
+package botengine
+
+const bufferSize = 1024
+
+type mockSource struct {
+	eventsChan chan *Event
+}
+
+func newMockSource() *mockSource { return &mockSource{eventsChan: make(chan *Event, bufferSize)} }
+
+func (m *mockSource) Events() <-chan *Event {
+	return m.eventsChan
+}
+
+func (m *mockSource) Close() error {
+	return nil
+}
+
+type mockSink struct {
+	flushedChan chan *Event
+}
+
+func newMockSink() *mockSink { return &mockSink{flushedChan: make(chan *Event, bufferSize)} }
+
+func (m *mockSink) Flush(ev *Event) error {
+	m.flushedChan <- ev
+	return nil
+}
+
+func (m *mockSink) Close() {}
